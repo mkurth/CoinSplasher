227b5921030c6117477920c16ac5f881f25cecf6
mkurth <mku@qu.do>
2018-11-06 14:55:33 +0100

    output is now with current worth in €

--- src/main/scala/com/mkurth/coinsplasher/portadapter/repo/console/ConsoleIO.scala
+++ src/main/scala/com/mkurth/coinsplasher/portadapter/repo/console/ConsoleIO.scala
@@ -3,20 +3,23 @@ package com.mkurth.coinsplasher.portadapter.repo.console
 import com.mkurth.coinsplasher.domain.{BuyOrder, Order, SellOrder}
 
 import scala.math.BigDecimal
+import scala.math.BigDecimal.RoundingMode
 import scala.util.Try
 
 trait ConsoleIO {
 
   def orderToString(order: Order): String = {
     order match {
-      case BuyOrder(coinSymbol, amount, _) => s"buy $coinSymbol $amount"
-      case SellOrder(coinSymbol, amount, _) => s"sell $coinSymbol $amount"
+      case BuyOrder(coinSymbol, amount, worth) =>
+        s"buy  $coinSymbol\t ${amount.setScale(10, RoundingMode.CEILING)}\t for ${worth.setScale(2, RoundingMode.CEILING)} €"
+      case SellOrder(coinSymbol, amount, worth) =>
+        s"sell $coinSymbol\t ${amount.setScale(10, RoundingMode.CEILING)}\t for ${worth.setScale(2, RoundingMode.CEILING)} €"
     }
   }
 
   def stringToOrder(in: String): Option[Order] = {
     Try {
-      val Array(orderType: String, coinSymbol: String, amount: String) = in.split(" ")
+      val Array(orderType: String, coinSymbol: String, amount: String) = in.split("[ \t]+").take(3)
       orderType match {
         case "buy" => BuyOrder(coinSymbol, BigDecimal(amount), 0)
         case "sell" => SellOrder(coinSymbol, BigDecimal(amount), 0)
--- src/test/scala/com/mkurth/coinsplasher/portadapter/repo/console/ConsoleIOTest.scala
+++ src/test/scala/com/mkurth/coinsplasher/portadapter/repo/console/ConsoleIOTest.scala
@@ -8,7 +8,7 @@ class ConsoleIOTest extends FlatSpec with Matchers {
 
   val consoleIo = new ConsoleIO {}
   "ConsoleIO" should "write BuyOrder" in {
-    consoleIo.orderToString(BuyOrder("BTC", 12, 0)) should be("buy BTC 12")
+    consoleIo.orderToString(BuyOrder("BTC", 12, 0)) should (include("buy") and include("BTC") and include("12"))
   }
 
   it should "read BuyOrder" in {
@@ -16,11 +16,11 @@ class ConsoleIOTest extends FlatSpec with Matchers {
   }
 
   it should "write SellOrder" in {
-    consoleIo.orderToString(SellOrder("XLM", 0.0012, 0)) should be("sell XLM 0.0012")
+    consoleIo.orderToString(SellOrder("XLM", 0.0012, 0)) should (include("sell") and include("XLM") and include("0.0012"))
   }
 
   it should "read SellOrder" in {
-    consoleIo.stringToOrder("sell OMG 0.00001") should be(Some(SellOrder("OMG", 0.00001, 0)))
+    consoleIo.stringToOrder("sell \t OMG\t 0.00001 for 10 €") should be(Some(SellOrder("OMG", 0.00001, 0)))
   }
 
   it should "not read invalid command" in {
